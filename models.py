17aef78fe679aa7926b7d86dc53d4afe9301ce15
chompatas <47321861+chompatas@users.noreply.github.com>
2019-02-05 17:08:08 -0200

    Update models.py

--- models.py
+++ models.py
@@ -2,8 +2,7 @@ from google.appengine.ext import db
  
 class BlogPost(db.Model):
     subject = db.StringProperty(required=True)
-    author = db.UserProperty(required=True,
-                             auto_current_user=True)
+    author = db.UserProperty(required=True, auto_current_user=True)
     content = db.TextProperty(required=True)
     created = db.DateTimeProperty(auto_now_add=True)
     last_updated = db.DateTimeProperty(auto_now=True)
